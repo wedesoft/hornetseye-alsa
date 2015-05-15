8e6b4089409c052504ba142add35fb0315269f0b
Jan Wedekind (Dr) <jan@wedesoft.de>
2015-05-15 22:25:00 +0100

    Update version

--- config.rb
+++ config.rb
@@ -1,7 +1,7 @@
 require 'rake'
 
 PKG_NAME = 'hornetseye-alsa'
-PKG_VERSION = '1.3.1'
+PKG_VERSION = '1.3.2'
 CFG = RbConfig::CONFIG
 CXX = ENV[ 'CXX' ] || 'g++'
 RB_FILES = ['config.rb'] + FileList[ 'lib/**/*.rb' ]
